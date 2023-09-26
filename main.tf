b98c17f0a5934893af85b2f64bd45e909c67a479
fikay <32597117+fikay@users.noreply.github.com>
2023-09-25 18:03:57 -0600

    22 restructure root module (#23)
    * Update Week-1.md
    
    * # change structure of root module and tagging bucket

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-terraform {
-  cloud {
-    organization = "FaksOrg"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   length = 32
@@ -39,9 +10,12 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Coonventions
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
+
+  tags = {
+    UserUuid =var.UserUuid
+  }
+
 }
 
-output "random_bucket_name_result"{
-    value = random_string.bucket_name.result
-}
+
 
