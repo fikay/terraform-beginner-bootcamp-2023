94be21c249757ecd629e1b9846bd6ce424f2158d
fikay <fikayofakeye@yahoo.com>
2023-09-28 00:18:32 +0000

    #34 Inalidate cloudfront stack when version changes

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "website_endpoint" {
   description = "Url for our static website hosting"
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "domain_name" {
+  description = "Cloudfront domain Name"
+  value = module.terrahouse_aws.cloudfront_domain
+}
