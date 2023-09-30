575bf4c19ca700987649cff526cacc8ab34ac095
fikay <fikayofakeye@yahoo.com>
2023-09-30 17:57:14 +0000

    try to fix tags

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -32,7 +32,7 @@ resource "aws_s3_object" "index_html" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("${path.root}${var.index_html_filepath}")
+  etag = filemd5("${var.index_html_filepath}")
 }
 
 resource "aws_s3_object" "error_html" {
@@ -44,7 +44,7 @@ resource "aws_s3_object" "error_html" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("${path.root}${var.error_html_filepath}")
+  etag = filemd5("${var.error_html_filepath}")
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
