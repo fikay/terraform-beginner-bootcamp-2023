15ef4e1d65c8896d09a7610e25f4a7beb1526c03
fikay <fikayofakeye@yahoo.com>
2023-10-06 14:02:12 +0000

    #40  Implement terraform provider block in terraform

--- variables.tf
+++ variables.tf
@@ -6,19 +6,19 @@ variable "bucket_Name" {
   type        = string
 }
 
-variable "index_html_filepath" {
-  type        = string
-}
+# variable "index_html_filepath" {
+#   type        = string
+# }
 
-variable "error_html_filepath" {
-  type        = string
-}
+# variable "error_html_filepath" {
+#   type        = string
+# }
 
-variable "content_version" {
-  type        = number
-}
+# variable "content_version" {
+#   type        = number
+# }
 
-variable "assets_path" {
-  type = string
+# variable "assets_path" {
+#   type = string
   
-}
+# }
